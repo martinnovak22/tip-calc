5c8696677ccd1171bdd0bb3d4e24cfb7a24d751a
martinnovak22 <99553117+martinnovak22@users.noreply.github.com>
2022-02-20 15:51:59 +0100

    Making funcionality

---
+++ script.js
@@ -0,0 +1,42 @@
+let bill = 0;
+let people = 1;
+let tip = 0;
+
+function getBill(el) {
+  bill = document.getElementById("bill-num").value;
+  bill = parseInt(bill, 10);
+  console.log(bill);
+  calculate();
+}
+
+function getPeople() {
+  people = document.getElementById("num-of-people").value;
+  people = parseInt(people, 10);
+  console.log(people);
+  calculate();
+}
+
+function getTip(el) {
+  tip = el.value;
+  if (isNaN(tip)) {
+    console.log("not a number");
+  } else {
+    tip = parseInt(tip, 10);
+    console.log(tip);
+    calculate();
+  }
+}
+
+let result_tip;
+
+function calculate() {
+  if (bill != 0 && people != 0 && tip != 0) {
+    let result_tip = ((bill / 100) * tip) / people;
+    let result_total = (bill + (bill / 100) * tip) / people;
+
+    outputTip.value = result_tip.toFixed(2);
+    outputTotal.value = result_total.toFixed(2);
+  } else {
+    console.log("Not ready for calculation");
+  }
+}
